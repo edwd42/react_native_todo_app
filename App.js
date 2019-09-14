713d363a2b7174f355b18815041419b41c56d58d
edwd42 <edwd42@yahoo.com>
2019-09-14 16:19:13 -0400

    change state.todo from string to array and iterate in renderTodos()

--- App.js
+++ App.js
@@ -3,22 +3,29 @@ import { StyleSheet, Text, TextInput, Button, View } from "react-native";
 
 export default class App extends React.Component {
   state = {
-    todo: "",
+    todo: ["Learn React Native", "Build Apps"],
     text: ""
   };
   addTodo = () => {
     this.setState({ todo: this.state.text });
   };
+
+  renderTodos = () => {
+    return this.state.todo.map(t => {
+      return <Text key={t}>{t}</Text>;
+    });
+  };
   render() {
     return (
       <View style={styles.container}>
         <Text>My Todo App</Text>
-        <Text>{this.state.todo}</Text>
+
         <TextInput
           style={styles.inputStyle}
           onChangeText={text => this.setState({ text })}
         />
         <Button title="Add New Todo" onPress={this.addTodo} />
+        {this.renderTodos()}
       </View>
     );
   }
