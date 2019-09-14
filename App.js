6c9659b13dffab751d1aad6913861c5a92c7b513
edwd42 <edwd42@yahoo.com>
2019-09-14 15:36:19 -0400

    add TextInput box

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>My Todo App</Text>
+      <TextInput style={styles.inputStyle} />
     </View>
   );
 }
@@ -12,8 +13,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
   },
+  inputStyle: {
+    height: 40,
+    width: "80%",
+    borderColor: "green",
+    borderWidth: 1
+  }
 });
